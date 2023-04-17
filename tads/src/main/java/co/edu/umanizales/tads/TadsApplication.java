869fa7e45e56a0aadbb105bed932ac7909fa94c0
JuanDiego500-sys <126423820+JuanDiego500-sys@users.noreply.github.com>
2023-04-17 16:40:05 -0500

    Add files via upload

---
+++ tads/src/main/java/co/edu/umanizales/tads/TadsApplication.java
@@ -0,0 +1,14 @@
+package co.edu.umanizales.tads;
+
+import co.edu.umanizales.tads.model.ListSE;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TadsApplication.class, args);
+    }
+
+}
