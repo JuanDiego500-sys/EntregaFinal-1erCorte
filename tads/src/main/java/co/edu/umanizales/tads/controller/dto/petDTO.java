9969f9520140a6b679355669a3ade73e458ba2e5
JuanDiego500-sys <126423820+JuanDiego500-sys@users.noreply.github.com>
2023-04-26 09:54:18 -0500

    Add files via upload

---
+++ tads/src/main/java/co/edu/umanizales/tads/controller/dto/petDTO.java
@@ -0,0 +1,11 @@
+package co.edu.umanizales.tads.controller.dto;
+import lombok.Data;
+
+@Data
+public class petDTO {
+    private byte age;
+    private String name;
+    private String race;
+    private String codeLocation;
+    private char gender;
+}//end of the petDTO------------------------------------
