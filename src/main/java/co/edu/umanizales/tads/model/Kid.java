49b3a218d7199bfac40ceaf1ab1039a575a202a0
JuanDiego500-sys <jdgiraldo98535@umanizales.edu.co>
2023-05-03 15:54:53 -0500

    javax validations and creation of the class gender

--- src/main/java/co/edu/umanizales/tads/model/Kid.java
+++ src/main/java/co/edu/umanizales/tads/model/Kid.java
@@ -2,14 +2,16 @@ package co.edu.umanizales.tads.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
 @Data
 @AllArgsConstructor
+
+
 public class Kid {
+
     private String identification;
     private String name;
     private byte age;
-    private char gender;
+    private Gender gender;
     private Location location;
 
 }
--- src/main/java/co/edu/umanizales/tads/model/Pet.java
+++ src/main/java/co/edu/umanizales/tads/model/Pet.java
@@ -2,15 +2,39 @@ package co.edu.umanizales.tads.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 public class Pet {
+    @NotBlank
+    @NotEmpty
+    @Positive
     private byte age;
+    @NotBlank
+    @NotEmpty
+    @Size(min=2, max = 50)
     private String name;
+    @NotBlank
+    @NotEmpty
+    @Size(min=2, max = 50)
     private String type;
+    @NotBlank
+    @NotEmpty
+    @Size(min=2, max = 50)
     private String race;
+    @Valid
+    @NotBlank
     private Location location;
-    private char gender;
+    @NotBlank
+    @NotEmpty
+    private Gender gender;
+    @Size(min = 6, max =15 )
+    @NotEmpty
+    @NotBlank
     private String ownerPhone;
 }//end of pet------------------------------------------
